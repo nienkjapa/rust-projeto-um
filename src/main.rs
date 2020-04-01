fdab08855b6533d8e75ba6a91c32313d3d96bead
rians-afk <riantranding2020@gmail.com>
2020-03-31 22:43:30 -0300

    rust: adicionando comentários
    comentários são essenciais para determinar/explicar o que fazemos no código.
    Signed-off-by: rians-afk <riantranding2020@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 fn main() {
     println!("Rian: Olá Alisson!");
-
-    another_function();
+// aqui começa a interação inicial do persongem Rian, 
+// que conversará com os seus amigos do Telegram na 
+// linguagem rust.
+    another_function(); // FAZEM PARTE DA FUNÇÃO
 }
 
-fn another_function() {
-    println!("Alisson Lauffer: Olá Rian!");
+fn another_function() { // FAZEM PARTE DA FUNÇÃO
+    println!("Alisson Lauffer: Olá Rian!"); // conversação em print, assemelha-se ao python
 
     println!("Renatoh: Também quero entrar na conversa! <3");
 }
+
